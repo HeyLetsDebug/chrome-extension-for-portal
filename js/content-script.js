3c2a1a8ff81f7cbd19d5af5416898cce4faa4dce
Abhishek Dubey <38434912+HeyLetsDebug@users.noreply.github.com>
2021-10-24 21:40:46 +0530

    Cleanup

--- js/content-script.js
+++
@@ -1,5 +0,0 @@
-// https://gskpro-com.preview-cf65.gskinternet.com/en-gb/therapy-areas/respiratory/
-// https://gskpro-com.preview-cf65.gskinternet.com/en-ca/products/zejula/
-// https://gskpro-com.preview-cf65.gskinternet.com/en-master/products/zejula-mt-new/recurrent-ovarian-cancer/dosing/
-
-chrome.action.onClicked.addListener(function(tab) { alert('icon clicked');console.log('icon clicked')});
